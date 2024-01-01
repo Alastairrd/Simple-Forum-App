e14b54447ed3e3be2ec103a08ce32d1b56aa425f
Alastairrd <alastairros-davison@hotmail.com>
2024-01-01 19:23:49 +0000

    updating sql files

--- create_db.sql
+++ create_db.sql
@@ -1,19 +1,94 @@
-CREATE DATABASE myForum;
+CREATE DATABASE  IF NOT EXISTS `myForum` ;
+USE `myForum`;
 
-use myForum;
+CREATE TABLE `posts` (
+  `post_id` int NOT NULL AUTO_INCREMENT,
+  `topic_id` int NOT NULL,
+  `title` varchar(360) NOT NULL,
+  `author_id` int NOT NULL,
+  `content` varchar(10000) DEFAULT NULL,
+  `like_count` int NOT NULL DEFAULT '0',
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB;
 
-CREATE table users (user_id INT NOT NULL auto_increment, username varchar(20), email VARCHAR(320), password_ VARCHAR(64), signup_date date, primary key (user_id));
-CREATE table topics (topic_id INT NOT NULL auto_increment, topic_name VARCHAR(45) not null, topic_desc VARCHAR(400), primary key (topic_id));
-CREATE table posts (post_id INT NOT NULL auto_increment, topic_id INT NOT NULL, title VARCHAR(360) not null, author_id int not null, content VARCHAR(10000), like_count int, primary key (post_id));
-CREATE table replies (reply_id int not null auto_increment, previous_id int not null, content VARCHAR(1000) not null, likes int, replied_to boolean not null default 0, primary key (reply_id));
-CREATE table subscriptions (user_id int not null, topic_id int not null);
+CREATE TABLE `replies` (
+  `reply_id` int NOT NULL AUTO_INCREMENT,
+  `previous_id` int DEFAULT NULL,
+  `content` varchar(1000) NOT NULL,
+  `likes` int NOT NULL DEFAULT '0',
+  `replied_to` tinyint(1) NOT NULL DEFAULT '0',
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `post_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`reply_id`)
+) ENGINE=InnoDB;
 
-ALTER TABLE `posts` 
-ADD COLUMN `datetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP AFTER `like_count`;
+CREATE TABLE `topics` (
+  `topic_id` int NOT NULL AUTO_INCREMENT,
+  `topic_name` varchar(45) NOT NULL,
+  `topic_desc` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`topic_id`)
+) ENGINE=InnoDB;
 
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `email` varchar(320) DEFAULT NULL,
+  `pass` varchar(64) DEFAULT NULL,
+  `signup_date` date DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB;
 
-CREATE USER 'forumuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ardforum2023';
-GRANT ALL PRIVILEGES ON myForum.* TO 'forumuser'@'localhost';
+CREATE TABLE `subscriptions` (
+  `user_id` int NOT NULL,
+  `topic_id` int NOT NULL
+) ENGINE=InnoDB;
 
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getComments`(IN postID int)
+BEGIN
+    SELECT replies.*, users.username
+    from replies
+    JOIN users
+    ON replies.user_id = users.user_id
+    WHERE postID = replies.post_id
+    
+    ORDER by datetime ASC;
+END ;;
 
+;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getPostData`(IN postID INT)
+BEGIN
+	SELECT * FROM post_topics
+    WHERE postID = post_id;
+END ;;
+
+;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getPosts`(IN input_id INT)
+BEGIN
+	SELECT * FROM post_topics
+    WHERE topic_id = input_id;
+END ;;
+
+;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectTopicByID`(IN input_id INT)
+BEGIN
+
+SELECT * from topics where topic_id = input_id;
+	
+END ;;
+
+;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `topTopics`()
+BEGIN
+Select topic_id, COUNT(topic_id) FROM subscriptions GROUP BY topic_id ORDER BY COUNT(topic_id) DESC;
+END ;;
+
+;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `validateUser`(IN inputUser VARCHAR(20), IN inputPass VARCHAR(64))
+BEGIN
+SELECT * from users
+WHERE inputUser = username && inputPass = pass;
+END ;;
 
